c18b4dfe73d8f41b5cbea302b66f368af7612b64
Jakub Jurkian <jakub@jurkian.com>
2017-08-04 18:29:55 +0200

    Add routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+import {
+   BrowserRouter as Router,
+   Route,
+   Link
+} from 'react-router-dom';
+
+import Home from '../Home/index';
+import About from '../About/index';
+
 import './app.css';
 
 class App extends Component {
@@ -7,7 +16,26 @@ class App extends Component {
    }
 
    render() {
-      return <div />;
+      return (
+         <Router>
+            <div className="container">
+               <ul>
+                  <li><Link to="/">Home</Link></li>
+                  <li><Link to="/about">About</Link></li>
+                  <li><Link to="/login">Login</Link></li>
+                  <li><Link to="/start">Panel</Link></li>
+                  <li><Link to="/accounts">Accounts</Link></li>
+                  <li><Link to="/transfers">Transfers</Link></li>
+                  <li><Link to="/cards">Cards</Link></li>
+                  <li><Link to="/transactions">Transactions</Link></li>
+                  <li><Link to="/help">Help</Link></li>
+               </ul>
+
+               <Route exact path="/" component={Home} />
+               <Route path="/about" component={About} />
+            </div>
+         </Router>
+      );
    }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './components/App/index';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
