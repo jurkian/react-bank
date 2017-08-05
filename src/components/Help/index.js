2b859962bb0a2f67f5f908d1a68e6abb603b9544
Jakub Jurkian <jakub@jurkian.com>
2017-08-05 19:15:36 +0200

    Add form and some text to /help

--- src/components/Help/index.js
+++ src/components/Help/index.js
@@ -3,7 +3,33 @@ import React from 'react';
 const Help = () => {
    return (
       <div className="container">
-      <h1>Help</h1>
+         <h1>Help</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae temporibus quasi natus consectetur illum perferendis commodi neque molestiae recusandae.</p>
+
+         <section>
+            <form>
+               <p>
+                  <label htmlFor="name">Your name</label>
+                  <input id="name" type="name" name="name" placeholder="Your name..." />
+               </p>
+               <p>
+                  <label htmlFor="email">Your email</label>
+                  <input id="email" type="email" name="email" placeholder="Your email..." />
+               </p>
+               <p>
+                  <label htmlFor="subject">Subject</label>
+                  <select id="subject" name="subject">
+                     <option>Subject 1</option>
+                     <option>Subject 2</option>
+                     <option>Subject 3</option>
+                  </select>
+               </p>
+
+               <p>
+                  <button>Send message</button>
+               </p>
+            </form>
+         </section>
       </div>
    );
 }
