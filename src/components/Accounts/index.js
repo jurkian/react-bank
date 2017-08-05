c174239aaa0019a1db19a89519f8110265d092bd
Jakub Jurkian <jakub@jurkian.com>
2017-08-05 22:05:54 +0200

    List opened accounts

--- src/components/Accounts/index.js
+++ src/components/Accounts/index.js
@@ -4,14 +4,23 @@ class Accounts extends Component {
    constructor() {
       super();
 
-      this.state = { accounts: '' };
+      this.state = { accounts: [] };
    }
 
    render() {
+
+      // Accounts
+      const accounts = this.state.accounts
+         .map(acc => <Account key={acc.id} {...acc} />);
+
       return (
          <div className="container">
             <h1>Accounts</h1>
             <p>We have {this.state.accounts.length} accounts right now!</p>
+
+            <ul>
+               {accounts}
+            </ul>
          </div>
       );
    }
@@ -25,5 +34,8 @@ class Accounts extends Component {
    }
 
 }
+
+// Single account
+const Account = (props) => <li>{props.id}, {props.type}, {props.sortcode}, {props.number}, {props.currency}, {props.balance}</li>;
 
 export default Accounts;
