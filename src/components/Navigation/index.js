9e90200d1d87a4b128f7dd3deaaf553a027ae2e6
Jakub Jurkian <jakub@jurkian.com>
2017-08-19 00:04:47 +0200

    Validate user's JWT token on every panel's route change

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,13 +4,52 @@ import { Link } from 'react-router-dom';
 import HeaderBar from './HeaderBar/index';
 import MainNavigation from './MainNavigation/index';
 
-const Navigation = () => {
-   return (
-      <nav className="col-xs-12">
-         <HeaderBar />
-         <MainNavigation />
-      </nav>
-   );
+class Navigation extends Component {
+   constructor(props) {
+      super(props);
+   }
+
+   render() {
+      return (
+         <nav className="col-xs-12">
+            <HeaderBar />
+            <MainNavigation />
+         </nav>
+      );
+   }
+   
+   componentDidMount() {
+      // When the component is ready, check if user is authenticated
+      // If not - redirect to homepage
+      if (!localStorage.getItem('user_token')) {
+         this.props.history.push('/');
+      }
+   }
+
+   componentDidUpdate(prevProps) {
+      if (this.props.location !== prevProps.location) {
+         this.onPanelRouteChanged();
+      }
+   }
+   
+   // On every panel route change, send a request to server to check if user is authenticated
+   onPanelRouteChanged() {
+
+      fetch('http://localhost:3001/clients/1', {
+         headers: new Headers({
+            'x-auth-token': localStorage.getItem('user_token')
+         })
+      })
+      .then(res => res.json())
+      .then(res => {
+         
+         // Get response and make a decision
+         // If not authenticated, redirect to homepage
+         if (false) {
+            this.props.history.push('/');
+         }
+      });
+   }
 }
 
 export default Navigation;
