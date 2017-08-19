455151fd2b360f44f0126f660dee7cd3de328588
Jakub Jurkian <jakub@jurkian.com>
2017-08-19 10:54:14 +0200

    Navigation: highlight active link

--- src/components/Navigation/MainNavigation/index.js
+++ src/components/Navigation/MainNavigation/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './style.css';
 
@@ -8,74 +8,74 @@ const MainNavigation = () => {
       <div className="main-navigation">
          <ul>
             <li>
-               <Link to="/panel">
+               <NavLink to="/panel" exact={true}>
                   <div className="main-nav-widget">
                      <div className="main-nav-box">
                         <i className="ion-home" />
                      </div>
                      <span>Home</span>
                   </div>
-               </Link>
+               </NavLink>
             </li>
             <li>
-               <Link to="/panel/accounts">
+               <NavLink to="/panel/accounts">
                   <div className="main-nav-widget">
                      <div className="main-nav-box">
                         <i className="ion-clipboard" />
                      </div>
                      <span>Accounts</span>
                   </div>
-               </Link>
+               </NavLink>
             </li>
             <li>
-               <Link to="/panel/transactions">
+               <NavLink to="/panel/transactions">
                   <div className="main-nav-widget">
                      <div className="main-nav-box">
                         <i className="ion-arrow-swap" />
                      </div>
                      <span>Transactions</span>
                   </div>
-               </Link>
+               </NavLink>
             </li>
             <li>
-               <Link to="/panel/cards">
+               <NavLink to="/panel/cards">
                   <div className="main-nav-widget">
                      <div className="main-nav-box">
                         <i className="ion-card" />
                      </div>
                      <span>Cards</span>
                   </div>
-               </Link>
+               </NavLink>
             </li>
             <li>
-               <Link to="/panel/messages">
+               <NavLink to="/panel/messages">
                   <div className="main-nav-widget">
                      <div className="main-nav-box">
                         <i className="ion-chatbox-working" />
                      </div>
                      <span>Messages</span>
                   </div>
-               </Link>
+               </NavLink>
             </li>
             <li>
-               <Link to="/panel/profile">
+               <NavLink to="/panel/profile">
                   <div className="main-nav-widget">
                      <div className="main-nav-box">
                         <i className="ion-person" />
                      </div>
                      <span>Profile</span>
                   </div>
-               </Link>
+               </NavLink>
             </li>
             <li>
-               <Link to="/panel/help">
+               <NavLink to="/panel/help">
                   <div className="main-nav-widget">
                      <div className="main-nav-box">
                         <i className="ion-help" />
                      </div>
                      <span>Help</span>
                   </div>
-               </Link>
+               </NavLink>
             </li>
          </ul>
       </div>
