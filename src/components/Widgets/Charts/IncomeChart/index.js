2aa755607e8f3e5c564a9383953b89bc86a1748e
Jakub Jurkian <jakub@jurkian.com>
2017-08-19 14:01:20 +0200

    Income change widget: simplify, fetch data from db

--- src/components/Widgets/Charts/IncomeChart/index.js
+++ src/components/Widgets/Charts/IncomeChart/index.js
@@ -8,30 +8,24 @@ class IncomeChart extends Component {
 
    render() {
       return (
-         <div>
-            <AreaChart width={900} height={300} data={this.props.data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-               <defs>
-                  <linearGradient id="colorIncome" x1="0" y1="0" x2="0" y2="1">
-                     <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-                     <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-                  </linearGradient>
-                  <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
-                     <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                     <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-                  </linearGradient>
-               </defs>
-               <XAxis dataKey="name" />
-               <YAxis />
-               <CartesianGrid strokeDasharray="3 3" />
-               <Tooltip />
-               <Area type="monotone" dataKey="income" stroke="#82ca9d" fillOpacity={1} fill="url(#colorIncome)" />
-               <Area type="monotone" dataKey="expenses" stroke="#8884d8" fillOpacity={1} fill="url(#colorExpenses)" />
-            </AreaChart>
-
-            <p>Income: {this.props.income} {this.props.currency}</p>
-            <p>Expenses: {this.props.expenses} {this.props.currency}</p>
-            <p>Balance: {this.props.balance} {this.props.currency}</p>
-         </div>
+         <AreaChart width={900} height={300} data={this.props.data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+            <defs>
+               <linearGradient id="colorIncome" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+                  <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+               </linearGradient>
+               <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                  <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+               </linearGradient>
+            </defs>
+            <XAxis dataKey="date" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area type="monotone" dataKey="income" stroke="#82ca9d" fillOpacity={1} fill="url(#colorIncome)" />
+            <Area type="monotone" dataKey="expenses" stroke="#8884d8" fillOpacity={1} fill="url(#colorExpenses)" />
+         </AreaChart>
       );
    }
 }
--- src/components/Widgets/IncomeStats/index.js
+++ src/components/Widgets/IncomeStats/index.js
@@ -5,37 +5,22 @@ class IncomeStats extends Component {
    constructor(props) {
       super(props);
 
-      this.state = {
-         account: '',
-         income: '',
-         expenses: '',
-         chartData: [
-            { name: '25.08.2017', income: 2400, expenses: 4000 },
-            { name: '26.08.2017', income: 1398, expenses: 3000 },
-            { name: '27.08.2017', income: 9800, expenses: 2000 },
-            { name: '28.08.2017', income: 3908, expenses: 2780 },
-            { name: '29.08.2017', income: 4800, expenses: 1890 },
-            { name: '30.08.2017', income: 3800, expenses: 2390 },
-            { name: '31.08.2017', income: 4300, expenses: 3490 }
-         ]
-      }
+      this.state = { account: '', chartData: [] };
    }
 
    render() {
       return (
          <div className="widget stats-widget">
             <h3>Income change stats</h3>
-               <select onChange={this.changeStatsRange.bind(this)} ref="statsRange">
+            <p><strong>Account: </strong> {this.state.account.id} {this.state.account.type}</p>
+            <p><strong>Balance: </strong> {this.state.account.balance} {this.state.account.currency}</p>
+
+            <select onChange={this.changeStatsRange.bind(this)} ref="statsRange">
                <option value="7">Last 7 days</option>
                <option value="30">Last 30 days</option>
             </select>
 
-            <IncomeChart
-               data={this.state.chartData}
-               income={this.state.income}
-               expenses={this.state.expenses}
-               balance={this.state.account.balance}
-               currency={this.state.account.currency} />
+            <IncomeChart data={this.state.chartData} />
          </div>
       );
    }
@@ -45,23 +30,19 @@ class IncomeStats extends Component {
       // Set income and expenses stats to 7 days, by default
       fetch('http://localhost:3001/accounts/1')
       .then(res => res.json())
-      .then(account => this.setState({ account, income: account.income_7_days, expenses: account.expenses_7_days }));
+      .then(account => this.setState({
+         account,
+         chartData: account.income_expenses_7_days
+      }));
    }
 
    changeStatsRange() {
       let statsRange = this.refs.statsRange.value;
 
       if (statsRange === '7') {
-         this.setState({
-            income: this.state.account.income_7_days,
-            expenses: this.state.account.expenses_7_days
-         });
-
+         this.setState({ chartData: this.state.account.income_expenses_7_days });
       } else if (statsRange === '30') {
-         this.setState({
-            income: this.state.account.income_30_days,
-            expenses: this.state.account.expenses_30_days
-         });
+         this.setState({ chartData: this.state.account.income_expenses_30_days });
       }
    }
 }
