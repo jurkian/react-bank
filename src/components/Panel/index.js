56363f83777c99935ba05974608b2a9b23d4eca2
Jakub Jurkian <jakub@jurkian.com>
2017-08-19 13:05:10 +0200

    Chart: move data to state, add new component

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import { AreaChart, linearGradient, XAxis, YAxis, CartesianGrid, Tooltip, Area } from 'recharts';
-
-const data = [
-  { name: "25.08.2017", income: 2400, expenses: 4000 },
-  { name: "26.08.2017", income: 1398, expenses: 3000 },
-  { name: "27.08.2017", income: 9800, expenses: 2000 },
-  { name: "28.08.2017", income: 3908, expenses: 2780 },
-  { name: "29.08.2017", income: 4800, expenses: 1890 },
-  { name: "30.08.2017", income: 3800, expenses: 2390 },
-  { name: "31.08.2017", income: 4300, expenses: 3490 }
-];
+import StatsWidget from './StatsWidget/index';
 
 class Panel extends Component {
    constructor() {
       super();
 
-      this.state = { client: '', account: '', income: '', expenses: '' }
+      this.state = {
+         client: '',
+         account: '',
+         income: '',
+         expenses: '',
+         chartData: [
+            { name: "25.08.2017", income: 2400, expenses: 4000 },
+            { name: "26.08.2017", income: 1398, expenses: 3000 },
+            { name: "27.08.2017", income: 9800, expenses: 2000 },
+            { name: "28.08.2017", income: 3908, expenses: 2780 },
+            { name: "29.08.2017", income: 4800, expenses: 1890 },
+            { name: "30.08.2017", income: 3800, expenses: 2390 },
+            { name: "31.08.2017", income: 4300, expenses: 3490 }
+         ]
+      }
    }
 
    render() {
@@ -32,28 +36,12 @@ class Panel extends Component {
                   <option value="30">Last 30 days</option>
                </select>
 
-               <AreaChart width={900} height={300} data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-                  <defs>
-                     <linearGradient id="colorIncome" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-                     </linearGradient>
-                     <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-                     </linearGradient>
-                  </defs>
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <Tooltip />
-                  <Area type="monotone" dataKey="income" stroke="#82ca9d" fillOpacity={1} fill="url(#colorIncome)" />
-                  <Area type="monotone" dataKey="expenses" stroke="#8884d8" fillOpacity={1} fill="url(#colorExpenses)" />
-               </AreaChart>
-
-               <p>Income: {this.state.income} {this.state.account.currency}</p>
-               <p>Expenses: {this.state.expenses} {this.state.account.currency}</p>
-               <p>Balance: {this.state.account.balance} {this.state.account.currency}</p>
+               <StatsWidget
+                  data={this.state.chartData}
+                  income={this.state.income}
+                  expenses={this.state.expenses}
+                  balance={this.state.account.balance}
+                  currency={this.state.account.currency} />
             </div>
          </div>
       );
