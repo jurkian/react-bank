1caa5976f7854033f11b38ec9b01b59fbfdce688
Jakub Jurkian <jakub.jurkian@gmail.com>
2017-12-03 13:17:37 +0100

    Add Ajax loader for Cards section

--- src/components/Cards/PINChange/index.js
+++ src/components/Cards/PINChange/index.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import LoadingAnimation from 'components/LoadingAnimation/index';
 import SingleModuleButton from 'components/Buttons/SingleModuleButton/index';
 
 class PINChange extends Component {
    constructor() {
       super();
 
-      this.state = { singleCard: [], validationInfo: '' };
+      this.state = { singleCard: [], validationInfo: '', isFetching: true };
    }
 
    render() {
       return (
          <div className="col-sm-6 col-sm-offset-3">
-            <h1>PIN change for {this.state.singleCard.id}. {this.state.singleCard.type} card</h1>
 
-            <form onSubmit={this.handleFormSubmit.bind(this)}>
-               <div className="form-group">
-                  <label htmlFor="new-pin">Enter new PIN</label>
-                  <input type="text" id="new-pin" name="new-pin" className="form-control" placeholder="Enter new PIN..." ref="newPin" required />
+            {this.state.isFetching ? (
+               <LoadingAnimation />
+            ) : (
+               <div>
+                  <h1>PIN change for {this.state.singleCard.id}. {this.state.singleCard.type} card</h1>
+
+                  <form onSubmit={this.handleFormSubmit.bind(this)}>
+                     <div className="form-group">
+                        <label htmlFor="new-pin">Enter new PIN</label>
+                        <input type="text" id="new-pin" name="new-pin" className="form-control" placeholder="Enter new PIN..." ref="newPin" required />
+                     </div>
+
+                     <div className="form-group">
+                        <label htmlFor="new-pin-conf">Confirm new PIN</label>
+                        <input type="text" id="new-pin-conf" name="new-pin-conf" className="form-control" placeholder="Confirm new PIN..." required />
+                     </div>
+
+                     <p className="validation-info">{this.state.validationInfo}</p>
+
+                     <SingleModuleButton text="Change PIN" type="submit" />
+                  </form>
                </div>
+            )}
 
-               <div className="form-group">
-                  <label htmlFor="new-pin-conf">Confirm new PIN</label>
-                  <input type="text" id="new-pin-conf" name="new-pin-conf" className="form-control" placeholder="Confirm new PIN..." required />
-               </div>
-
-               <p className="validation-info">{this.state.validationInfo}</p>
-
-               <SingleModuleButton text="Change PIN" type="submit" />
-            </form>
          </div>
       );
    }
@@ -38,7 +46,7 @@ class PINChange extends Component {
       axios.get(`http://localhost:3001/cards/${this.props.match.params.cardId}`)
       .then(res => res.data)
       .then(singleCard => {
-         this.setState({ singleCard });
+         this.setState({ singleCard, isFetching: false });
       });
    }
 
--- src/components/Cards/Single/index.js
+++ src/components/Cards/Single/index.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import LoadingAnimation from 'components/LoadingAnimation/index';
 import CardInfobox from 'components/Infobox/CardInfobox/index';
 
 class SingleCard extends Component {
    constructor() {
       super();
 
-      this.state = { singleCard: [] };
+      this.state = { singleCard: [], isFetching: true };
    }
 
    render() {
       return (
          <div className="row">
-            <div className="col-xs-12">
-               <CardInfobox {...this.state.singleCard} currentUrl={this.props.match.url} />
-            </div>
+
+            {this.state.isFetching ? (
+               <LoadingAnimation />
+            ) : (
+               <div className="col-xs-12">
+                  <CardInfobox {...this.state.singleCard} currentUrl={this.props.match.url} />
+               </div>
+            )}
+
          </div>
       );
    }
@@ -24,7 +30,7 @@ class SingleCard extends Component {
       axios.get(`http://localhost:3001/cards/${this.props.match.params.cardId}`)
       .then(res => res.data)
       .then(singleCard => {
-         this.setState({ singleCard });
+         this.setState({ singleCard, isFetching: false });
       });
    }
 }
