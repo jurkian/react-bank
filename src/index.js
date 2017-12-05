51f52da2e38389c0926f0df106895e3c22e3bdb0
Jakub Jurkian <jakub.jurkian@gmail.com>
2017-12-05 23:58:57 +0100

    Connect Redux with App, enable Dev Tools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import './polyfills.js';
-
 import './vendor/bootstrap/custom-bootstrap.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
-import App from './components/App/index';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+   reducers,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+   <Provider store={store}>
+      <App />
+   </Provider>,
+   document.getElementById('root')
+);
 
 registerServiceWorker();
