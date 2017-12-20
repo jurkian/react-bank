18883ab99d142edb7e9aca39d975b35ea8bbf160
Jakub Jurkian <jakub.jurkian@gmail.com>
2017-12-20 22:31:49 +0100

    Improve routing for Transactions

--- src/components/Transactions/index.js
+++ src/components/Transactions/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import TransactionsList from './List/index';
 import SingleTransaction from './Single/index';
@@ -8,8 +8,10 @@ const Transactions = (props) => {
    return (
       <div className="row">
          <div className="col-xs-12">
-            <Route exact path={props.match.url} component={TransactionsList} />
-            <Route path={`${props.match.url}/:transId`} component={SingleTransaction} />
+            <Switch>
+               <Route exact path="/panel/transactions" component={TransactionsList} />
+               <Route path="/panel/transactions/:transId" component={SingleTransaction} />
+            </Switch>
          </div>
       </div>
    );
