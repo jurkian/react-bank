4ac5b4988767b9236f119d677be9168679704e37
Jakub Jurkian <jakub.jurkian@gmail.com>
2017-12-23 15:02:34 +0100

    CurrencyStats: add date picker (from airbnb)

--- src/components/CurrencyStats/CurrencySettings/index.js
+++ src/components/CurrencyStats/CurrencySettings/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import moment from 'moment';
+import { SingleDatePicker, isInclusivelyBeforeDay } from 'react-dates';
 
+import 'react-dates/lib/css/_datepicker.css';
 import './style.css';
 
-const CurrencySettings = (props) => {
-   return (
-      <div>
-         Choose currencies and date...
-      </div>
-   );
-}
+import 'react-dates/initialize';
 
-CurrencySettings.propTypes = {}
+class CurrencySettings extends Component {
+   constructor() {
+      super();
+      this.state = { date: moment().subtract(1, 'day'), focused: false };
+   }
+   
+   render() {
+      return (
+         <div>
+            <p>Choose base currency and date...</p>
+
+            <form className="currency-settings-form">
+               <div className="form-group">
+                  <input type="text" className="form-control" placeholder="Base currency..." />
+               </div>
+
+               <div className="form-group">
+                  <SingleDatePicker
+                     date={this.state.date} // momentPropTypes.momentObj or null
+                     onDateChange={date => this.setState({ date })} // PropTypes.func.isRequired
+                     focused={this.state.focused} // PropTypes.bool
+                     onFocusChange={({ focused }) => this.setState({ focused })} // PropTypes.func.isRequired
+                     showDefaultInputIcon={true}
+                     renderCalendarInfo={() => true}
+                     isOutsideRange={day => !isInclusivelyBeforeDay(day, moment().subtract(1, 'day'))}
+                  />
+               </div>
+            </form>
+         </div>
+      );
+   }
+}
 
 export default CurrencySettings;
