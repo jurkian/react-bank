32c21be709a91502225057e32493dd1e31fa1e9e
Jakub Jurkian <jakub.jurkian@gmail.com>
2018-03-15 11:10:45 +0100

    Prepare structure for toggle/remove messages

--- src/components/Messages/ListElement/index.js
+++ src/components/Messages/ListElement/index.js
@@ -2,15 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import './style.css';
+
 const MessagesListEl = ({ matchUrl, id, title, date }) => {
    return (
-      <Link to={`${matchUrl}/${id}`} className="list-group-item">
-         <h4 className="list-group-item-heading">{id}. {title}</h4>
-         <p className="list-group-item-text">
-            date: {date}
-         </p>
+      <Link to={`${matchUrl}/${id}`} className="list-group-item messages-list-item">
+         <div>
+            <h4 className="list-group-item-heading">{id}. {title}</h4>
+            <p className="list-group-item-text">
+               date: {date}
+            </p>
+         </div>
+         <aside>
+            <div className="checkbox-container" onClick={handleCheckboxClick}>
+               <input type="checkbox" />
+            </div>
+            <button className="btn btn-danger btn-sm" onClick={handleRemoveClick}>Remove</button>
+         </aside>
       </Link>
    );
+}
+
+const handleCheckboxClick = (e) => {
+   e.preventDefault();
+}
+
+const handleRemoveClick = (e) => {
+   e.preventDefault();
 }
 
 MessagesListEl.propTypes = {
