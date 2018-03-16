49dae1cd933c353539e99358690e908af54dbc27
Jakub Jurkian <jakub.jurkian@gmail.com>
2018-03-16 04:38:25 +0100

    Pagination: add fully working prototype

--- src/components/Messages/List/index.js
+++ src/components/Messages/List/index.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { messageToggle, messageRemove } from 'actions/messages';
+import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 import MessagesListEl from '../ListElement';
 
 class MessagesList extends Component {
-   constructor() {
-      super();
+   constructor(props) {
+      super(props);
 
-      this.state = { search: '' };
+      const perPage = 8;
+
+      this.state = {
+         search: '',
+         data: [],
+         url: 'http://localhost:3001/messages',
+         currentPage: 1,
+         perPage,
+         pageCount: Math.ceil(props.messagesCount / perPage)
+      };
+   }
+
+   componentWillMount() {
+      // Set default data based on loaded messages
+      // Load first (perPage) items
+      this.setState({ data: this.props.messages.filter((el, i) => i < this.state.perPage) });
+   }
+
+   // Internal Ajax, because pagination is only local and doesn't affect global state
+   fetchPaginationData() {
+      axios(`${this.state.url}?_page=${this.state.currentPage}&_limit=${this.state.perPage}`, {
+         method: 'get',
+         headers: { 'Content-Type': 'application/json' }
+      })
+         .then(res => res.data)
+         .then(data => this.setState({ data }))
+         .catch(error => { });
+   }
+
+   handlePageClick = ({ selected }) => {
+      // On click: fetch new data from API, basing on selected page
+      // Always increment selected page, because pages in API start from 1
+      this.setState({ currentPage: selected + 1 }, () => {
+         this.fetchPaginationData();
+      });
+   };
+
+   findMessage() {
+      this.setState({ search: this.refs.search.value });
    }
 
    render() {
       // Messages
       // Allow search for message title
-      const messages = this.props.messages
+      const messages = this.state.data
          .filter(message => message.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1)
          .map(message => {
             return (
@@ -31,7 +71,7 @@ class MessagesList extends Component {
          <div>
             <h1>Messages</h1>
 
-            <p>There are {this.props.messages.length} messages in your box</p>
+            <p>There are {this.props.messagesCount} messages in your box</p>
 
             <div className="form-group">
                <input
@@ -44,18 +84,30 @@ class MessagesList extends Component {
             <div className="list-group">
                {messages}
             </div>
+
+            <div className="pagination-container">
+               <ReactPaginate
+                  pageCount={this.state.pageCount}
+                  pageRangeDisplayed={3}
+                  marginPagesDisplayed={2}
+                  previousLabel={'Previous'}
+                  nextLabel={'Next'}
+                  breakLabel={<span>...</span>}
+                  onPageChange={this.handlePageClick}
+                  containerClassName={'pagination'}
+                  activeClassName={'active'}
+                  disableInitialCallback={true}
+               />
+            </div>
          </div>
       );
-   }
-
-   findMessage() {
-      this.setState({ search: this.refs.search.value });
    }
 }
 
 const mapStateToProps = (state) => {
    return {
-      messages: state.messages.data
+      messages: state.messages.data,
+      messagesCount: state.messages.data.length
    }
 };
 
