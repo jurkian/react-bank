d166b37d29e81206f8c95a7401bda550d371c46a
Jakub Jurkian <jakub.jurkian@gmail.com>
2018-04-13 13:27:50 +0200

    Enable Redux DevTools only in dev mode

--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ import './vendor/custom-bootstrap.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import reducers from 'reducers';
+import rootReducer from 'reducers';
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(
-   reducers,
-   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+// Activate Redux DevTools only in dev mode
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const store = createStore(rootReducer, composeEnhancers(
    applyMiddleware(thunk)
-);
+));
 
 ReactDOM.render(
    <Provider store={store}>
