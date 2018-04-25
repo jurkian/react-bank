1bc74ad0f738b880c5e02a957d13ee1d27467ad1
Jakub Jurkian <jakub.jurkian@gmail.com>
2018-04-25 16:19:22 +0200

    Reduce delay on Loader

--- src/components/UI/Loader/LoadingAnimation/index.js
+++ src/components/UI/Loader/LoadingAnimation/index.js
@@ -6,7 +6,7 @@ import './style.scss';
 const LoadingAnimation = () => {
    return (
       <div className="loading-animation">
-         <ReactLoading color="#3c6cde" type="spokes" width={64} height={64} delay={300} />
+         <ReactLoading color="#3c6cde" type="spokes" width={64} height={64} delay={100} />
       </div>
    );
 };
