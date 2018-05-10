9e89625404a4f9711a873bc13c5246bdb90ee7ef
Jakub Jurkian <jakub.jurkian@gmail.com>
2018-05-10 21:21:59 +0200

    Initialize Firebase and Firestore

---
+++ src/components/App/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+   apiKey: 'AIzaSyBH89-DxQWuo7xVc3zi48h1I6IewVOU0R4',
+   authDomain: 'react-bank-f7cc8.firebaseapp.com',
+   databaseURL: 'https://react-bank-f7cc8.firebaseio.com',
+   projectId: 'react-bank-f7cc8',
+   storageBucket: 'react-bank-f7cc8.appspot.com',
+   messagingSenderId: '576297099205'
+};
