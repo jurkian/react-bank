ccb754218183a0f9379e0d8c902c37191c7651a7
Jakub Jurkian <jakub.jurkian@gmail.com>
2018-05-14 20:01:37 +0200

    Get user's initial data: simplify it for 1 user support (more in the future)

--- src/tools/firebase/getUserInitialData.js
+++ src/tools/firebase/getUserInitialData.js
@@ -17,99 +17,87 @@ const initialData = {
 
 // Users
 const users = email =>
-   new Promise((resolve, reject) => {
-      db
-         .collection('users')
-         .where('email', '==', email)
-         .limit(1)
-         .get()
-         .then(data => {
-            // User data
-            initialData.user = data.docs[0].data();
-            initialData.user.id = data.docs[0].id;
-            userId = data.docs[0].id;
-
-            resolve();
-         })
-         .catch(err => reject(err));
-   });
+   db
+      .collection('users')
+      .where('email', '==', email)
+      .limit(1)
+      .get()
+      .then(data => {
+         // User data
+         initialData.user = data.docs[0].data();
+         initialData.user.id = data.docs[0].id;
+         userId = data.docs[0].id;
+      })
+      .catch(err => err);
 
 // Accounts
 const accounts = () =>
-   new Promise(resolve => {
-      db
-         .collection('accounts')
-         .where('user_id', '==', userId)
-         .limit(10)
-         .get()
-         .then(accounts => {
-            // Get accounts data
-            initialData.accounts = accounts.docs.map(doc => {
-               accIds.push(doc.id);
+   db
+      .collection('accounts')
+      .where('user_id', '==', userId)
+      .limit(10)
+      .get()
+      .then(accounts => {
+         // Get accounts data
+         initialData.accounts = accounts.docs.map(doc => {
+            accIds.push(doc.id);
 
-               return {
-                  ...doc.data(),
-                  id: doc.id
-               };
-            });
-         })
-         .then(() => {
-            for (const accId of accIds) {
-               // Transactions (we need account IDs)
-               db
-                  .collection('transactions')
-                  .where('source_acc_id', '==', accId)
-                  .limit(10)
-                  .get()
-                  .then(transactions => {
-                     // Get transactions
-                     initialData.transactions = transactions.docs.map(doc => ({
-                        ...doc.data(),
-                        id: doc.id
-                     }));
-                  });
-
-               // Cards (we need account IDs)
-               db
-                  .collection('cards')
-                  .where('user_id', '==', userId)
-                  .limit(10)
-                  .get()
-                  .then(cards => {
-                     // Get cards
-                     initialData.cards = cards.docs.map(doc => ({
-                        ...doc.data(),
-                        id: doc.id
-                     }));
-                  });
-            }
-
-            resolve();
+            return {
+               ...doc.data(),
+               id: doc.id
+            };
          });
-   });
+      });
+
+// Transactions (we will need account IDs - in the future!)
+const transactions = () =>
+   db
+      .collection('transactions')
+      .limit(10)
+      .get()
+      .then(transactions => {
+         // Get transactions
+         initialData.transactions = transactions.docs.map(doc => ({
+            ...doc.data(),
+            id: doc.id
+         }));
+      });
+
+// Cards (we need user ID)
+const cards = () =>
+   db
+      .collection('cards')
+      .where('user_id', '==', userId)
+      .limit(10)
+      .get()
+      .then(cards => {
+         // Get cards
+         initialData.cards = cards.docs.map(doc => ({
+            ...doc.data(),
+            id: doc.id
+         }));
+      });
 
 // Messages
 const messages = () =>
-   new Promise(resolve => {
-      db
-         .collection('messages')
-         .where('user_id', '==', userId)
-         .limit(10)
-         .get()
-         .then(messages => {
-            // Get messages
-            initialData.messages = messages.docs.map(doc => ({
-               ...doc.data(),
-               id: doc.id
-            }));
-
-            resolve();
-         });
-   });
+   db
+      .collection('messages')
+      .where('user_id', '==', userId)
+      .limit(10)
+      .get()
+      .then(messages => {
+         // Get messages
+         initialData.messages = messages.docs.map(doc => ({
+            ...doc.data(),
+            id: doc.id
+         }));
+      });
 
 // Return Promise with initialData
 export default email =>
    users(email)
       .then(accounts)
+      .then(transactions)
+      .then(cards)
       .then(messages)
       .then(() => initialData);
