9d62541bd236781a23b5d8b7ca46feafb5df7892
Jakub Jurkian <jakub.jurkian@gmail.com>
2018-05-14 14:38:27 +0200

    New transaction: no need to check if accounts loaded (they have!)

--- src/components/Transactions/New/index.js
+++ src/components/Transactions/New/index.js
@@ -1,45 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from 'actions';
-import Loader from 'components/UI/Loader';
 import Form from './Form';
 
 class NewTransaction extends Component {
    render() {
-      if (!this.props.fetchAccountsStatus) {
-         return <Loader />;
-      } else {
-         const accounts = this.props.accounts;
-         const userAccountsList = Object.entries(accounts).map(([key, acc]) => (
-            <option key={key} value={key}>
-               {`${acc.type}, ${acc.sortcode}, ${acc.balance} ${acc.currency}`}
-            </option>
-         ));
+      const accounts = this.props.accounts;
+      const userAccountsList = Object.entries(accounts).map(([key, acc]) => (
+         <option key={key} value={key}>
+            {`${acc.type}, ${acc.sortcode}, ${acc.balance} ${acc.currency}`}
+         </option>
+      ));
 
-         return (
-            <div className="row">
-               <div className="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                  <section className="new-transfer module">
-                     <h1>New transfer</h1>
+      return (
+         <div className="row">
+            <div className="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+               <section className="new-transfer module">
+                  <h1>New transfer</h1>
 
-                     <Form
-                        userAccountsList={userAccountsList}
-                        firstAccId={this.props.firstAccId}
-                        addTransaction={this.props.addTransaction}
-                     />
-                  </section>
-               </div>
+                  <Form
+                     userAccountsList={userAccountsList}
+                     firstAccId={this.props.firstAccId}
+                     addTransaction={this.props.addTransaction}
+                  />
+               </section>
             </div>
-         );
-      }
+         </div>
+      );
    }
 }
 
 const mapStateToProps = state => {
    return {
       accounts: state.accounts.data,
-      firstAccId: Object.keys(state.accounts.data)[0],
-      fetchAccountsStatus: state.accounts.status
+      firstAccId: Object.keys(state.accounts.data)[0]
    };
 };
 
