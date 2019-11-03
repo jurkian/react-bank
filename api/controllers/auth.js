b71d4192d28871814917a9fe922485c183a267a9
Jakub Jurkian <jakub.jurkian@gmail.com>
2019-11-03 14:09:50 +0100

    Add /login controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -24,8 +24,40 @@ exports.register = async (req, res, next) => {
          throwError('Problems creating a user', 422);
       }
 
-      //  sendWelcomeEmail()
+      // Send welcome email + verification link (expires in 7 days)
+      // sendNotification('email', 'welcome', {
+      //    to: user.email,
+      //    firstName: user.firstName
+      // });
+
       res.status(201).json({ message: 'User has been created' });
+   } catch (err) {
+      passError(err, next);
+   }
+};
+
+// Login user
+exports.login = async (req, res, next) => {
+   try {
+      const { email, password } = req.body;
+      let user = await User.findOne({ email });
+
+      if (!user) {
+         throwError('A user with this email could not be found', 422);
+      }
+
+      let isPassOk = await bcrypt.compare(password, user.password);
+
+      if (!isPassOk) {
+         throwError('Wrong password!', 401);
+      }
+
+      const { id } = user;
+      const token = jwt.sign({ id, email }, CONFIG.jwt_secret_key, {
+         expiresIn: '1d'
+      });
+
+      res.status(200).json({ token });
    } catch (err) {
       passError(err, next);
    }
