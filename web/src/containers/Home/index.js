0d852a87897efa4dd1f1680a3cf55ea37ffbb822
Jakub Jurkian <jakub.jurkian@gmail.com>
2019-11-08 10:29:35 +0100

    Fix: get user's count from common endpoint

--- web/src/containers/Home/index.js
+++ web/src/containers/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getUsersCount } from 'api/users';
+import { getUsersCount } from 'api/common';
 
 import Loader from 'components/UI/Loader';
 import Header from 'components/Home/Header';
