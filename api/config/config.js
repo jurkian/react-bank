9d3593e2d0ae457ecc82ce40755820a51fff25a9
Jakub Jurkian <jakub.jurkian@gmail.com>
2019-11-14 18:07:24 +0100

    Conditionally set Mongo to debug mode

--- api/config/config.js
+++ api/config/config.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 let CONFIG = {};
 
+// Current ENV
+CONFIG.current_env = process.env.CURRENT_ENV || 'production';
+
 // Port
 CONFIG.port = process.env.PORT || '3001';
 
--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -1,5 +1,8 @@
 const CONFIG = require('@config/config');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose').set(
+   'debug',
+   CONFIG.current_env === 'development' ? true : false
+);
 const chalk = require('chalk');
 
 mongoose.connect(`${CONFIG.mongodb_uri}`, {
