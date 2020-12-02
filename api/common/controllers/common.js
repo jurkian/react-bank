cfb0808b106efd57fa0ac47d14f3b79bcf1de95b
Jakub Jurkian <jakub.jurkian@gmail.com>
2020-12-03 00:21:04 +0100

    Get app config: return some Strapi settings

--- api/common/controllers/common.js
+++ api/common/controllers/common.js
@@ -1,12 +1,22 @@
 'use strict';
 
+const _ = require('lodash');
+
 // Get app config
 const getAppConfig = async ctx => {
-   console.log(strapi);
+   const pluginStore = await strapi.store({
+      environment: '',
+      type: 'plugin',
+      name: 'users-permissions'
+   });
 
-   debugger;
+   const settings = await pluginStore.get({
+      key: 'advanced'
+   });
 
-   // ...
+   const settingsToReturn = _.pick(settings, ['allow_register', 'email_confirmation']);
+
+   return ctx.send(settingsToReturn);
 };
 
 module.exports = {
