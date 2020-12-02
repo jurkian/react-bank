9d1197e444ed9526359fe661d1d8999716b1e1d4
Jakub Jurkian <jakub.jurkian@gmail.com>
2020-12-03 00:10:57 +0100

    Stats controller: prepare basics

--- api/stats/controllers/stats.js
+++ api/stats/controllers/stats.js
@@ -1,8 +1,9 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+const getMyStats = async ctx => {
+   // ...
+};
 
-module.exports = {};
+module.exports = {
+   getMyStats
+};
