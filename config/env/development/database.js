3f856266e5264fe3a247f6e5c58d8766e9e051d5
Jakub Jurkian <jakub.jurkian@gmail.com>
2020-12-02 20:59:01 +0100

    Add dev/prod database config

---
+++ config/env/development/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+   defaultConnection: "default",
+   connections: {
+      default: {
+         connector: "bookshelf",
+         settings: {
+            client: "mysql",
+            host: env("DEV_DATABASE_HOST", "localhost"),
+            port: env.int("DEV_DATABASE_PORT", 3306),
+            database: env("DEV_DATABASE_NAME", "strapi"),
+            username: env("DEV_DATABASE_USERNAME"),
+            password: env("DEV_DATABASE_PASSWORD"),
+            ssl: env.bool("DEV_DATABASE_SSL", false),
+         },
+         options: {},
+      },
+   },
+});
---
+++ config/env/production/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+   defaultConnection: "default",
+   connections: {
+      default: {
+         connector: "bookshelf",
+         settings: {
+            client: "mysql",
+            host: env("PROD_DATABASE_HOST", "localhost"),
+            port: env.int("PROD_DATABASE_PORT", 3306),
+            database: env("PROD_DATABASE_NAME", "strapi"),
+            username: env("PROD_DATABASE_USERNAME"),
+            password: env("PROD_DATABASE_PASSWORD"),
+            ssl: env.bool("PROD_DATABASE_SSL", false),
+         },
+         options: {},
+      },
+   },
+});
