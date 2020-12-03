017d4a712fb6fbe15d6a441ae944c7e30a24640b
Jakub Jurkian <jakub.jurkian@gmail.com>
2020-12-03 01:48:05 +0100

    Add logic to send form

--- api/form/controllers/form.js
+++ api/form/controllers/form.js
@@ -2,7 +2,23 @@
 
 // Send form
 const sendForm = async ctx => {
-   // ...
+   try {
+      const data = ctx.request.body;
+      const settings = await strapi.query('settings').findOne();
+      const formSendTo = settings.send_form_emails_to || 'email@example.com';
+
+      await strapi.plugins['email'].services.email.send({
+         to: formSendTo,
+         from: data.from,
+         subject: `Contact form email from ${data.from}`,
+         text: data.content
+      });
+
+      ctx.send({ ok: true });
+   } catch (error) {
+      console.log(error);
+      debugger;
+   }
 };
 
 module.exports = {
