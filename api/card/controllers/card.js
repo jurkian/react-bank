40a0856e33623e207b5796436504d57dd828bc25
Jakub Jurkian <jakub.jurkian@gmail.com>
2020-12-03 01:35:35 +0100

    Card: add update pin and update limits logic

--- api/card/controllers/card.js
+++ api/card/controllers/card.js
@@ -39,7 +39,27 @@ const getSingle = async ctx => {
 // Change card PIN
 const changePin = async ctx => {
    try {
-      // ...
+      const currentUser = ctx.state.user;
+      const { cardId } = ctx.params;
+      const data = ctx.request.body;
+
+      const myCard = await strapi.query('card').findOne({ id: cardId, owner: currentUser.id });
+
+      if (!myCard) {
+         return ctx.throw(400, 'no-card');
+      }
+
+      // Updates
+      const updates = {};
+
+      if (data.new_pin) {
+         updates.pin = data.new_pin;
+      }
+
+      // Final save
+      const updatedCard = await strapi.query('card').update({ id: myCard.id }, { ...updates });
+
+      ctx.send(updatedCard);
    } catch (error) {
       console.log(error);
       debugger;
@@ -49,7 +69,43 @@ const changePin = async ctx => {
 // Change card limits
 const changeLimits = async ctx => {
    try {
-      // ...
+      const currentUser = ctx.state.user;
+      const { cardId } = ctx.params;
+      const data = ctx.request.body;
+
+      const myCard = await strapi.query('card').findOne({ id: cardId, owner: currentUser.id });
+
+      if (!myCard) {
+         return ctx.throw(400, 'no-card');
+      }
+
+      // Updates
+      const updates = {};
+
+      // Daily online limit
+      if (data.dol) {
+         updates.daily_online_limit = data.dol;
+      }
+
+      // Daily withdrawal limit
+      if (data.dwl) {
+         updates.daily_withdrawal_limit = data.dwl;
+      }
+
+      // Monthly online limit
+      if (data.mol) {
+         updates.monthly_online_limit = data.mol;
+      }
+
+      // Monthly withdrawal limit
+      if (data.mwl) {
+         updates.monthly_withdrawal_limit = data.mwl;
+      }
+
+      // Final save
+      const updatedCard = await strapi.query('card').update({ id: myCard.id }, { ...updates });
+
+      ctx.send(updatedCard);
    } catch (error) {
       console.log(error);
       debugger;
