00b8073e617d4a1940399c77605f0323c1885fa4
Jakub Jurkian <jakub.jurkian@gmail.com>
2020-12-03 18:39:13 +0100

    Add Transfer validations, update Strapi Model

---
+++ api/transfer/controllers/create-valids.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+// allowed: lowercase and uppercase letters, space, '
+const nameRegex = /^[ a-zA-Z']+$/;
+
+// allowed: lowercase and uppercase letters, numbers, space, -
+const addrRegex = /^[ a-zA-Z0-9\-]+$/;
+
+module.exports = Joi.object({
+   payee_name: Joi.string().regex(nameRegex).min(2).max(50).trim().required(),
+
+   amount: Joi.number().precision(2).positive().required(),
+
+   currency: Joi.string().valid('eur', 'gbp').trim().required(),
+
+   reference: Joi.string().regex(nameRegex).min(2).max(40).trim().required(),
+
+   source_account: Joi.number().integer().positive().required(),
+
+   payee_acc_number: Joi.number().integer().positive().max(30).required(),
+
+   payee_addr: Joi.string().regex(addrRegex).min(10).max(100).trim().required(),
+
+   type: Joi.string().valid('normal', 'turbo').trim().required()
+});
