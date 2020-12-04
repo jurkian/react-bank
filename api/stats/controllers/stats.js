51b5bfb9606cc31507ffe511b408ff4893e5eb7a
Jakub Jurkian <jakub.jurkian@gmail.com>
2020-12-04 13:52:26 +0100

    Get and return user's stats

--- api/stats/controllers/stats.js
+++ api/stats/controllers/stats.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const getMyStats = async ctx => {
-   // ...
+   try {
+      const getStats = await strapi.query('stats').findOne();
+
+      if (!getStats) {
+         strapi.services.errors.throwError(400, 'No stats available, use dummy data creator');
+      }
+
+      // Stats: income, expenses for specific user
+      const incomeStatsRes = getStats.income_stats
+         .filter(el => el.type === 'income')
+         .map(el => ({ collected_at: el.collected_at, value: el.value }));
+      const expensesStatsRes = getStats.expenses_stats
+         .filter(el => el.type === 'expenses')
+         .map(el => ({ collected_at: el.collected_at, value: el.value }));
+
+      return {
+         incomeStats: incomeStatsRes,
+         expensesStats: expensesStatsRes
+      };
+   } catch (error) {
+      strapi.services.errors.throwError(400, error.message);
+   }
 };
 
 module.exports = {
