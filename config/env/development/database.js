3abbd12fc5329b6aafc5b6195917a2849cd65780
Jakub Jurkian <jakub.jurkian@gmail.com>
2020-12-04 13:48:57 +0100

    Update default DB name

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-   defaultConnection: "default",
+   defaultConnection: 'default',
    connections: {
       default: {
-         connector: "bookshelf",
+         connector: 'bookshelf',
          settings: {
-            client: "mysql",
-            host: env("DEV_DATABASE_HOST", "localhost"),
-            port: env.int("DEV_DATABASE_PORT", 3306),
-            database: env("DEV_DATABASE_NAME", "strapi"),
-            username: env("DEV_DATABASE_USERNAME"),
-            password: env("DEV_DATABASE_PASSWORD"),
-            ssl: env.bool("DEV_DATABASE_SSL", false),
+            client: 'mysql',
+            host: env('DEV_DATABASE_HOST', 'localhost'),
+            port: env.int('DEV_DATABASE_PORT', 3306),
+            database: env('DEV_DATABASE_NAME', 'react-bank-api'),
+            username: env('DEV_DATABASE_USERNAME'),
+            password: env('DEV_DATABASE_PASSWORD'),
+            ssl: env.bool('DEV_DATABASE_SSL', false)
          },
-         options: {},
-      },
-   },
+         options: {}
+      }
+   }
 });
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-   defaultConnection: "default",
+   defaultConnection: 'default',
    connections: {
       default: {
-         connector: "bookshelf",
+         connector: 'bookshelf',
          settings: {
-            client: "mysql",
-            host: env("PROD_DATABASE_HOST", "localhost"),
-            port: env.int("PROD_DATABASE_PORT", 3306),
-            database: env("PROD_DATABASE_NAME", "strapi"),
-            username: env("PROD_DATABASE_USERNAME"),
-            password: env("PROD_DATABASE_PASSWORD"),
-            ssl: env.bool("PROD_DATABASE_SSL", false),
+            client: 'mysql',
+            host: env('PROD_DATABASE_HOST', 'localhost'),
+            port: env.int('PROD_DATABASE_PORT', 3306),
+            database: env('PROD_DATABASE_NAME', 'react-bank-api'),
+            username: env('PROD_DATABASE_USERNAME'),
+            password: env('PROD_DATABASE_PASSWORD'),
+            ssl: env.bool('PROD_DATABASE_SSL', false)
          },
-         options: {},
-      },
-   },
+         options: {}
+      }
+   }
 });
