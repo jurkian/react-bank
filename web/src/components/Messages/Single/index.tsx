560a0b6d2fc21916165fdd573962e1da3b180bfd
Jakub Jurkian <jakub.jurkian@gmail.com>
2021-07-15 13:43:02 +0200

    TS: rewrite Messages -> Single, ListElement

--- web/src/components/Messages/Single/index.tsx
+++ web/src/components/Messages/Single/index.tsx
@@ -1,28 +1,39 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router-dom';
+import { useAppSelector } from '@hooks';
+
 import { formatDate } from 'tools';
 
-const SingleMessage = ({ singleMessage: { title, sentDate, content } }) => {
-   sentDate = formatDate(sentDate, 'dd/MM/yyyy HH:mm');
+interface Params {
+   messageId: string;
+}
+
+interface Props extends RouteComponentProps<Params> {}
+
+type Message = {
+   _id: string;
+   title: string;
+   sentDate: Date;
+   content: string;
+};
+
+const SingleMessage: React.FC<Props> = (props) => {
+   const singleMessage: Message = useAppSelector((state) =>
+      state.messages.data.find((el) => el._id === messageId)
+   );
+   const messageId = props.match.params.messageId;
+   const sentDate = formatDate(singleMessage?.sentDate, 'dd/MM/yyyy HH:mm');
 
    return (
       <section className="module single-message">
-         <h1>{title}</h1>
+         <h1>{singleMessage.title}</h1>
          <p>Date: {sentDate}</p>
 
          <hr />
 
-         <article dangerouslySetInnerHTML={{ __html: content }} />
+         <article dangerouslySetInnerHTML={{ __html: singleMessage.content }} />
       </section>
    );
 };
 
-const mapStateToProps = (state, ownProps) => {
-   const messageId = ownProps.match.params.messageId;
-
-   return {
-      singleMessage: state.messages.data.find(el => el._id === messageId)
-   };
-};
-
-export default connect(mapStateToProps)(SingleMessage);
+export default SingleMessage;
