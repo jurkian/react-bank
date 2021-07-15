eb96d2fa139dca1f006eeb0de67b38b1679d6733
Jakub Jurkian <jakub.jurkian@gmail.com>
2021-07-15 13:21:01 +0200

    TS: rewrite Loader and LoadingAnimation

--- web/src/components/UI/Loader/LoadingAnimation/index.tsx
+++ web/src/components/UI/Loader/LoadingAnimation/index.tsx
@@ -3,7 +3,7 @@ import ReactLoading from 'react-loading';
 
 import './style.scss';
 
-const LoadingAnimation = () => {
+const LoadingAnimation: React.FC = () => {
    return (
       <div className="loading-animation">
          <ReactLoading color="#3c6cde" type="spokes" width={64} height={64} delay={100} />
--- web/src/components/UI/Loader/index.tsx
+++ web/src/components/UI/Loader/index.tsx
@@ -1,30 +1,30 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoadingAnimation from './LoadingAnimation';
 
-class Loader extends Component {
-   state = { showError: false };
+const Loader: React.FC = () => {
+   const [showError, setShowError] = useState(false);
 
-   render() {
-      return (
-         <Fragment>
-            <LoadingAnimation />
-            {this.state.showError ? (
-               <p>If loading takes too long, please refresh the page...</p>
-            ) : null}
-         </Fragment>
-      );
-   }
+   let errorTimeout: ReturnType<typeof setTimeout>;
 
-   componentDidMount() {
+   useEffect(() => {
       // Start timeout to show the error message
-      this.errorTimeout = setTimeout(() => this.setState({ showError: true }), 3000);
-   }
+      errorTimeout = setTimeout(() => setShowError(true), 3000);
 
-   componentWillUnmount() {
-      // Remove the timeout when component will be unmounted
-      // Otherwise it will still be working in background
-      clearTimeout(this.errorTimeout);
-   }
-}
+      return () => {
+         // Cleanup
+         // Remove the timeout when component will be unmounted
+         // Otherwise it will still be working in background
+         clearTimeout(errorTimeout);
+      };
+   }, []);
+
+   return (
+      <>
+         <LoadingAnimation />
+
+         {showError ? <p>If loading takes too long, please refresh the page...</p> : null}
+      </>
+   );
+};
 
 export default Loader;
